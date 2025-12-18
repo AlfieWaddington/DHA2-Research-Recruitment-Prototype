48a4e9ec2bd472b846911bc6074b5243f02e7dd2
Alfie Waddington <alfiework0212@outlook.com>
2025-12-18 07:43:57 +0000

    Streak Cards

---
+++ app/participant/studies/Journey.tsx
@@ -0,0 +1,30 @@
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+
+export function Journey(){
+
+
+
+
+    return (
+            <>
+                <div className="pl-1 pr-1">
+
+                    
+
+
+
+
+
+
+
+
+
+
+                </div>
+
+            </>
+
+    )
+
+
+}
--- app/participant/studies/ProgressHeader.tsx
+++ app/participant/studies/ProgressHeader.tsx
@@ -1,5 +1,6 @@
 import {Card, CardHeader, CardBody, CardFooter} from "@heroui/card"
-
+import { Flame } from "lucide-react";
+import { Star } from "lucide-react";
 
 export function ProgressHeader(){
 
@@ -7,10 +8,10 @@ export function ProgressHeader(){
 
     return (
         <>
-            <div className="pl-5 pr-5">
+            <div className="pl-5 pr-5 justify-start">
 
                 
-                <Card className="justify-start flex flex-col rounded-lg shadow bg-white dark:bg-gray-800" radius="lg">
+                <Card className="flex flex-col rounded-lg shadow bg-white dark:bg-gray-800" radius="lg">
 
                     <CardHeader className="justify-start flex-1/3 grid grid-cols-12">
                         <h1 className="sm:col-span-4">Your Study Journey</h1> <div className="sm:col-span-6"></div> <h2 className="sm:col-span-1 text-center">###</h2> <div className="sm:col-span-1"></div>
@@ -18,16 +19,34 @@ export function ProgressHeader(){
 
                     </CardHeader>
 
-                    <CardBody className="justify-end flex-2/3 grid-cols-2">
+                    <CardBody className="justify-end flex-2/3 grid grid-cols-2">
                         
-                        <Card>
+                        <Card className="rounded-lg shadow bg-linear-to-b from-yellow-500 to-red-500">
+                            
+                            <CardHeader>
+                                <Flame /> <h4> # Task Streak!</h4>
+                            </CardHeader>
 
+
+                            <CardBody>
+                                <p className= "text-body-small">
+                                    Complete your tasks by their due dates to maintain your streak
+                                </p>
+                            </CardBody>
 
                         </Card>
 
-                        <Card>
+                        <Card className="rounded-lg shadow bg-linear-to-r from-purple-500 to-pink-500">
+                            <CardHeader>
+                                <Star /> <h4> #&#215; Bonus</h4>
+                            </CardHeader>
 
 
+                            <CardBody>
+                                <p className= "text-body-small">
+                                    Reward multiplier based on your highest streak
+                                </p>
+                            </CardBody>
 
                         </Card>
 
